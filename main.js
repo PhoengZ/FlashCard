0cf0ee99b890d62cdb763a75d5ca2d041cb26d38
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-02-09 13:38:22 +0700

    Fix can click on reveal button before vocabulary generate

--- main.js
+++ main.js
@@ -5,10 +5,14 @@ let Answer = document.querySelector("#Answer");
 let rev = document.querySelector("#reveal");
 let nq = document.querySelector("#next-question");
 
-let question_now = getQuestion();
+let question_now = "";
+rev.disabled = true;
+ab.style.display  = "none";
+getQuestion();
 
 async function getQuestion() {
     nq.disabled = true;
+    rev.disabled = true;
     let response = await fetch("https://random-word-api.herokuapp.com/word");
     let word = await response.json();
     while (await getAnswer(word[0],false) === "Not found"){
@@ -18,6 +22,7 @@ async function getQuestion() {
     question.innerText = word[0];
     question_now = word[0];
     nq.disabled = false;
+    rev.disabled = false;
 }
 async function getAnswer(word,b){
     let response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
@@ -28,8 +33,8 @@ async function getAnswer(word,b){
     if (b)Answer.innerText = ans[0].meanings[0].definitions[0].definition;
     return "Found";
 }
-
 rev.addEventListener("click",async()=>{
+    if (rev.disabled) return;
     await getAnswer(question_now,true);
     qb.style.display = "none";
     ab.style.display  = "flex";
@@ -40,4 +45,3 @@ nq.addEventListener("click", async ()=>{
     ab.style.display  = "none";
 });
 
-ab.style.display  = "none";
